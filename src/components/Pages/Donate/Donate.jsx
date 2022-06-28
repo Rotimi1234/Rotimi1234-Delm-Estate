cf22f63d4e47817efbb55affb925bdd8f7f2a0f3
dami_codes <80917622+Rotimi1234@users.noreply.github.com>
2022-06-28 23:44:32 +0100

    Add files via upload

---
+++ src/components/Pages/Donate/Donate.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../../navbar/Navbar'
+import Footer from '../Home/Sections/Footer'
+
+const Contact = () => {
+  return (
+    <div>
+      <Navbar/>
+      
+      <Footer/>
+    </div>
+  )
+}
+
+export default Contact
